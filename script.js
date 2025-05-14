8740c41de9d30e5d0da3446ee5c3a093c08ff334
Helsing22 <helsingrpp2@gmail.com>
2025-05-14 17:15:18 -0400

    Fix and cart with delivery

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function sendMessage(carName) {
+function calculateTotal(carName, carPrice) {
+    const deliveryCost = 500; // Costo de entrega
+    const totalPrice = carPrice + deliveryCost;
+    // Muestra el precio total en un alert
+    alert(`El precio total de ${carName} con entrega es: $${totalPrice}`);
+    // Habilita el botón para enviar mensaje
+    document.getElementById('sendMessageButton').onclick = function() {
+        sendMessage(carName, totalPrice);
+    };
+}
+function sendMessage(carName, totalPrice) {
     const phoneNumber = '13057761543'; // Reemplaza con el número de teléfono deseado
-    const message = `Hola, estoy interesado en ${carName}.`;
+    const address = document.getElementById('address').value;
+    const message = `Hola, estoy interesado en ${carName}. Precio total: $${totalPrice}. Mi dirección es: ${address}.`;
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
+
