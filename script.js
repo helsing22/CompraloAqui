9d8db08c97e77c041c5de22a4e0a1f5139e18779
Helsing22 <helsingrpp2@gmail.com>
2025-05-14 16:25:53 -0400

    PC reset 1 fix phone numb

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function sendMessage(carName) {
-    const phoneNumber = '1234567890'; // Reemplaza con el número de teléfono deseado
+    const phoneNumber = '13057761543'; // Reemplaza con el número de teléfono deseado
     const message = `Hola, estoy interesado en ${carName}.`;
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
